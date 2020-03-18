2b144e21674932006f82274fd7eb59f3c9e190bb
Mark Ghebrial <mghebrialjr@icloud.com>
2020-03-18 15:49:44 -0700

    Madness.

--- src/com/gmail/ghebrial/mark/InterfacePanel.java
+++ src/com/gmail/ghebrial/mark/InterfacePanel.java
@@ -1,59 +1,59 @@
 package com.gmail.ghebrial.mark;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 
 public class InterfacePanel extends JPanel {
-    private static final int WIDTH = 300, HEIGHT = 200;
 
-    private static StudentFile[] periodArray = new StudentFile[7];
-    private static StudentFile period1 = null;
-    private static StudentFile period2 = null;
-    private static StudentFile period3 = null;
-    private static StudentFile period4 = null;
-    private static StudentFile period5 = null;
-    private static StudentFile period6 = null;
-    private static StudentFile period7 = null;
+    //This array will be in the drop-down menu to select the period
+    private StudentFile[] periodArray = new StudentFile[7];
 
+    //IPMORTANT, THE PROGRAM WILL NOT WORK UNLESS YOU CHANGE THIS VARIABLE TO THE FILEPATH WITH THE .txt FILES OF THE NAMES
+    //TODO: Make this automatic somehow
+    private String rootPath = "D:\\Student Picker\\NameLists";
 
-    //IMPORTANT: Change this to the filepath of the folder with the lists of names
-    //TODO: Automatically do this
-    private static String filepathOfNameLists = "D://Student Picker//NameLists";
-
-    static {
-        try {
-            periodArray[0] = new StudentFile("Period 1",filepathOfNameLists + "//period1");
-            periodArray[1] = new StudentFile("Period 2",filepathOfNameLists + "//period2");
-            periodArray[2] = new StudentFile("Period 3",filepathOfNameLists + "//period3");
-            periodArray[3] = new StudentFile("Period 4",filepathOfNameLists + "//period4");
-            periodArray[4] = new StudentFile("Period 5",filepathOfNameLists + "//period5");
-            periodArray[5] = new StudentFile("Period 6",filepathOfNameLists + "//period6");
-            periodArray[6] = new StudentFile("Period 7",filepathOfNameLists + "//period7");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static JComboBox periodMenu = new JComboBox(periodArray);
-    private static JLabel studentLabel = new JLabel("Press button to choose a random student");
-    private static JButton nextButton = new JButton("Next Student");
+    //Create the graphic elements for the GUI
+    private JTextField filepathBox = new JTextField(rootPath);
+    private JComboBox periodMenu = new JComboBox(periodArray);
+    private JLabel studentLabel = new JLabel("Press button to choose a random student");
+    private JButton nextButton = new JButton("Next Student");
 
     public InterfacePanel() {
 
-        //periodList.
+        LoadFiles(rootPath);
 
-        nextButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                studentLabel.setText(periodArray[periodMenu.getSelectedIndex()].generateName());
-            }
+        //Run this code when nextButton is pressed
+        nextButton.addActionListener(actionEvent -> {
+            //Display a random name from the selected file
+            studentLabel.setText(periodArray[periodMenu.getSelectedIndex()].generateName());
         });
 
+        //Run this code when the user clicks enter in filepathBox
+        filepathBox.addActionListener(actionEvent -> {
+            //Reload the files
+            LoadFiles(filepathBox.getText());
+        });
+
+        //Add the graphic elements to the GUI
+        add(filepathBox);
         add(periodMenu);
         add(nextButton);
         add(studentLabel);
     }
 
+    private void LoadFiles (String rootPath) {
+        try {
+            periodArray[0] = new StudentFile("Period 1",rootPath + "//period1");
+            periodArray[1] = new StudentFile("Period 2",rootPath + "//period2");
+            periodArray[2] = new StudentFile("Period 3",rootPath + "//period3");
+            periodArray[3] = new StudentFile("Period 4",rootPath + "//period4");
+            periodArray[4] = new StudentFile("Period 5",rootPath + "//period5");
+            periodArray[5] = new StudentFile("Period 6",rootPath + "//period6");
+            periodArray[6] = new StudentFile("Period 7",rootPath + "//period7");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        periodMenu = new JComboBox(periodArray); //Update the list
+    }
 }
